0c549d696ddc041aabadda11a81f5b2b29c34369
Rafael <rafaelmolladouglas@gmail.com>
2018-04-06 22:36:55 -0300

    Icons for each channel were changed

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,27 @@
-$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/OgamingSC2?callback=?', function(data) {
-  console.log(data);
-});
+var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+
+for (var i = 0; i < channels.length; i++) {
+	$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + channels[i] +'?callback=?', function(data) {
+	 	
+		// If channel is transmiting(not null)
+		if (data.stream != null) {
+			
+		}
+			// Add class 'online' to channel element
+			// Add the correct information to each text next to the icons
+
+		// If channel isn't transmitting
+			// Add class 'offline' to channel element 
+			// We delete its views and followers icons
+
+	});
+}
+
+// When online button is clicked
+	// Remove elements with class .online
+
+// When offline button is clicked
+	// Remove elements with class .offline
+
+// When all button is clicked
+	// Show all elements
