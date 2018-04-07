fa414175277641de2eb91a09fcd627736e84161a
Rafael <rafaelmolladouglas@gmail.com>
2018-04-07 12:56:59 -0300

    Channels are now marked as online or offline

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,50 @@
-var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+var channels = ["ESL_SC2", "freecodecamp", "OgamingSC2", "cretetion", "noobs2ninjas", "RobotCaleb", "storbeck", "habathcx"];
 
-for (var i = 0; i < channels.length; i++) {
-	$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + channels[i] +'?callback=?', function(data) {
-	 	
-		// If channel is transmiting(not null)
+channels.forEach(function(channel, index) {
+	$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + channel +'?callback=?', function(data) {
+
+		console.log(data);
+
+		console.log(index);
+		// If channel is streaming(not null)
 		if (data.stream != null) {
-			
-		}
-			// Add class 'online' to channel element
-			// Add the correct information to each text next to the icons
+			// Add class 'online' to current channel
+			$( '#' + channel ).addClass( "online" );
 
-		// If channel isn't transmitting
-			// Add class 'offline' to channel element 
-			// We delete its views and followers icons
+			// Change channel info div to green indicating that this channel is streaming now
+			$("#" + channel + ' .channel-info').css( "background-color", "lightgreen" );
+
+			// Add more info about online channels
+			$('#' + channel + ' .views').append("<span>" + data.stream.channel.views + "</span");
+			$('#' + channel + ' .users').append("<span>" + data.stream.channel.followers + "</span");
+
+
+		} else {
+			// If channel isn't streaming, add class 'offline' to current channel
+			$('#' + channel).addClass( "offline" );
+
+			// Change channel info div to red indicating that this channel is streaming now
+			$("#" + channel + ' .channel-info').css( "background-color", "indianred" );
+
+		}
 
 	});
-}
+
+})
 
 // When online button is clicked
-	// Remove elements with class .online
+$( "#online" ).click(function() {
+	
+	// Remove background from previous section
+	$("#all").removeClass("selected");
+
+	// Add background to online
+	$(this).addClass("selected");
+
+	// Remove elements with class .offline
+	$( ".offline" ).remove();
+
+});
 
 // When offline button is clicked
 	// Remove elements with class .offline
